04e9682c121bcb186f1ae4673b8c0848af279fbe
deeksha01032023 <deekshapandey@cedcoss.com>
2023-06-22 17:10:03 +0530

    done

--- src/config/data.php
+++ src/config/data.php
@@ -1,20 +1,3 @@
 <?php
 return
-$marks = array(
-    "Student 1" => array(
-        "Roll" => 101,
-        "Maths" => 65,
-        "Science" => 94,
-    ),
-    "Student 2" => array(
-        "Roll" => 102,
-        "Maths" => 87,
-        "Science" => 81,
-    ),
-    "Student 3" => array(
-        "Roll" => 103,
-        "Maths" => 91,
-        "Science" => 68,
-    ),
-
-);
+$a=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
