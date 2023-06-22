f1e57b57f30c682f5fae5f3c146c611042f438de
deeksha01032023 <deekshapandey@cedcoss.com>
2023-06-22 17:03:32 +0530

    done

--- src/config/data.php
+++ src/config/data.php
@@ -1,5 +1,20 @@
-<?php return array (
-  'a' => 'red222',
-  'b' => 'green ',
-  'c' => 'blue',
+<?php
+return
+$marks = array(
+    "Student 1" => array(
+        "Roll" => 101,
+        "Maths" => 65,
+        "Science" => 94,
+    ),
+    "Student 2" => array(
+        "Roll" => 102,
+        "Maths" => 87,
+        "Science" => 81,
+    ),
+    "Student 3" => array(
+        "Roll" => 103,
+        "Maths" => 91,
+        "Science" => 68,
+    ),
+
 );
--- src/index.php
+++ src/index.php
@@ -1,22 +1,57 @@
-
 <h3>Original array</h3>
 
-<table border="1">
-  <caption>Data</caption>
-  <thead>
-    <tr>
-      <th scope="col">Key</th>
-      <th scope="col">Name</th>
-      <th scope="col">Edit Action</th>
-      <th scope="col">Delete Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-     $arr = require "config/data.php";
-     foreach ($arr as $key => $value)  {
-      echo "<tr><td>$key</td><td>$value</td><td><a href='/edit.php?id=$key&&value=$value'>Edit</a></td><td>
-     <a href='/delete.php?id=$key'>Delete</a><t/d></tr>";
-    }
 
-    ?>
+<?php
+$total = 0;
+$arr = require "config/data.php";
+foreach ($arr as $key => $value) {
+  echo $key;
+  echo "<br>";
+  foreach ($value as $vkey => $vvalue) {
+    if ($key == "Student 1" && ($vkey == "Maths" || $vkey == "Science")) {
+      $total1 += $vvalue;
+    }
+    if ($key == "Student 2" && ($vkey == "Maths" || $vkey == "Science")) {
+      $total2 += $vvalue;
+    }
+    if ($key == "Student 3" && ($vkey == "Maths" || $vkey == "Science")) {
+      $total3 += $vvalue;
+    }
+    if ($key == "Student 1" && ($vkey == "Science")) {
+      $totals1 += $vvalue;
+    }
+    if ($key == "Student 2" && ($vkey == "Science")) {
+      $totals2 += $vvalue;
+    }
+    if ($key == "Student 3" && ($vkey == "Science")) {
+      $totals3 += $vvalue;
+    }
+    if ($key == "Student 1" && ($vkey == "Maths")) {
+      $totalm1 += $vvalue;
+    }
+    if ($key == "Student 2" && ($vkey == "Maths")) {
+      $totalm2 += $vvalue;
+    }
+    if ($key == "Student 3" && ($vkey == "Maths")) {
+      $totalm3 += $vvalue;
+    }
+    echo "<br>";
+    echo $vkey;
+    echo "<br>";
+    echo $vvalue;
+    echo "<br>";
+  }
+  echo "<br>";
+}
+
+
+$max = ($total1 > $total2) ? (($total1 > $total3) ? $total1 : $total3) : (($total2 > $total3) ? $total2 : $total3);
+$maxm = ($totalm1 > $totalm2) ? (($totalm1 > $totalm3) ? $totalm1 : $totalm3) : (($totalm2 > $totalm3) ? $totalm2 : $totalm3);
+$maxs = ($totals1 > $totals2) ? (($totals1 > $totals3) ? $totals1 : $totals3) : (($totals2 > $totals3) ? $totals2 : $totals3);
+
+
+echo "Maximum is :$max for Student 2";
+echo "Maximum is :$maxm for Student 3";
+echo "Maximum is :$maxs for Student 1";
+
+?>
